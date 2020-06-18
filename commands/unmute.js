3894f92de64ecfcbc3e6ca568c8cf8b7e1e3c5db
Pitohuibot <67033903+Pitohuibot@users.noreply.github.com>
2020-06-18 16:27:08 -0300

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js')
 module.exports.run = async (bot, message, args) => {
     let muteRole = message.guild.roles.find("name", "Mutado");
     let member = message.mentions.members.first();
-    if(!member) return message.channel.send(`Mencione o usuário que deseja desmutar!`);
+    if(!member) return message.channel.send(` ${message.author} Mencione o usuário que deseja desmutar!`);
      else{
      member.removeRole(muteRole);
        let pEmbed = new Discord.RichEmbed()
