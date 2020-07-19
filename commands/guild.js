d7ff3b46cd051c8a63aabf0057336833829d0bc8
Pitohuibot <67033903+Pitohuibot@users.noreply.github.com>
2020-07-19 05:34:48 -0300

    Update guild.js

--- commands/guild.js
+++ commands/guild.js
@@ -1,10 +1,12 @@
+
 module.exports.run = async (client, message, args) => {
+    if(!message.member.hasID('335193454034419715')) return message.reply("nope")
     const guildArray = client.guilds.map((guild) => {
     return `Nome: ${guild.name}\nID: ${guild.id}\nPessoas: ${guild.memberCount}\n\n`
     })
 
     await message.author.send(`\`\`\`${guildArray.join("\n")}\`\`\``)
-    return message.channel.send("Mandei no seu privado !")
+    return message.channel.send("Lansei no pv")
 }
 
 module.exports.help = {
