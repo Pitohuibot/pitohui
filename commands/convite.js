515af6c6b84c6f0d0c99a28bf69eff24ff68f8b8
Pitohuibot <67033903+Pitohuibot@users.noreply.github.com>
2020-07-20 18:10:03 -0300

    Create convite.js

---
+++ commands/convite.js
@@ -0,0 +1,5 @@
+const Discord = require('discord.js')
+
+module.exports.run = async (client, message, args) => {
+  message.reply("Convite para o meu servidor de suporte!! https://discord.gg/HDrtKTu ");
+}
