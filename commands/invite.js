6d98947335294162ff233a1c231409a1477a183f
Pitohuibot <67033903+Pitohuibot@users.noreply.github.com>
2020-07-20 18:09:19 -0300

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js')
 
 module.exports.run = async (client, message, args) => {
-  message.reply("Convite para o meu servidor de suporte!! https://discord.gg/HDrtKTu ");
+  message.reply("Invite for my support server!! https://discord.gg/HDrtKTu ");
 }
